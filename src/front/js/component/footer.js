769c9e1ea6be6caadd438aabd343054e63104e60
cesarsan86 <86737129+cesarsan86@users.noreply.github.com>
2024-02-14 21:44:45 +0000

    navbar y footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+	<footer className="footer mt-auto py-5">
+		<div className="container">
+			<div className="row">
+        		<div className="col-md-4 text-left">
+					<p>Oficinas centrales</p>
+					<p className="text-primary">+34 935 351 260</p>
+					<p>Atención al paciente</p>
+					<p className="text-primary">+34 935 351 260</p>
+					<p className="text-primary">info@barzanitas.es</p>
+   				</div>
+				<div className="col-md-4 text-center">
+					<p>Horarios</p>
+					<p><strong>Lunes a Viernes de 9:00-14:00 y 16:00-20:00</strong></p>
+				</div>
+				<div className="col-md-4 d-flex flex-column justify-content-end align-items-end">
+  					<div className="me-4">
+    					<p>Síguenos en:</p>
+					</div>	
+    				<div className="">
+						<i className="fab fa-instagram fa-2x mx-2"></i>
+						<i className="fab fa-twitter fa-2x mx-2"></i>
+						<i className="fab fa-facebook fa-2x mx-2"></i>
+					</div>	
+				</div>	
+
+			</div>
+    	</div>
+  </footer>
+
 );
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LogoBarzanitas from "../../img/LogoBarzanitas.png";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img src={LogoBarzanitas} alt="Logo de Barzanitas"></img>
+					<span className="navbar-brand mb-0 h1">Barzanitas</span>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+				
+				
+				
+				<div className="dropdown">
+					
+						<button type="button" className="btn btn-primary me-3">Soy Cliente</button>
+						<button type="button" className="btn btn-success me-3">Soy Doctor</button>
+						<button type="button" className="btn btn-danger me-5">Cerrar Sesión</button>
+					
+						<a className="fa fa-bars fa-2x" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+						
+						</a>
+
+					<ul className="dropdown-menu dropdown-menu-end p-3 fs-5">
+						<li><a className="dropdown-item fs-5 py-3" href="#">Especialidades</a></li>
+						<li><a className="dropdown-item fs-5 py-3" href="#">Doctores</a></li>
+						<li><a className="dropdown-item fs-5 py-3" href="#">La empresa</a></li>
+						<li><a className="dropdown-item fs-5 py-3" href="#">Contáctanos</a></li>
+						
+					</ul>
 				</div>
+				
+					
 			</div>
 		</nav>
 	);
