f19773f437d590de99236b897d96469669be24ca
cesarsan86 <86737129+cesarsan86@users.noreply.github.com>
2024-02-15 13:21:38 +0000

    pagina principal y alguna modificación en navbar y footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,33 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-5">
-		<div className="container">
-			<div className="row">
-        		<div className="col-md-4 text-left">
-					<p>Oficinas centrales</p>
-					<p className="text-primary">+34 935 351 260</p>
-					<p>Atención al paciente</p>
-					<p className="text-primary">+34 935 351 260</p>
-					<p className="text-primary">info@barzanitas.es</p>
-   				</div>
-				<div className="col-md-4 text-center">
-					<p>Horarios</p>
-					<p><strong>Lunes a Viernes de 9:00-14:00 y 16:00-20:00</strong></p>
-				</div>
-				<div className="col-md-4 d-flex flex-column justify-content-end align-items-end">
-  					<div className="me-4">
-    					<p>Síguenos en:</p>
-					</div>	
-    				<div className="">
-						<i className="fab fa-instagram fa-2x mx-2"></i>
-						<i className="fab fa-twitter fa-2x mx-2"></i>
-						<i className="fab fa-facebook fa-2x mx-2"></i>
-					</div>	
-				</div>	
-
-			</div>
-    	</div>
+  <footer className="footer mt-auto py-5 bg-light">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4 mb-4">
+          <p>Oficinas centrales</p>
+          <p className="text-primary">+34 935 351 260</p>
+          <p>Atención al paciente</p>
+          <p className="text-primary">+34 935 351 260</p>
+          <p className="text-primary">info@barzanitas.es</p>
+        </div>
+        <div className="col-md-4 mb-4 text-center">
+          <p>Horarios</p>
+          <p>
+            <strong>Lunes a Viernes de 9:00-14:00 y 16:00-20:00</strong>
+          </p>
+        </div>
+        <div className="col-md-4 d-flex flex-column justify-content-end align-items-end">
+          <div className="mb-2">
+            <p>Síguenos en:</p>
+          </div>
+          <div>
+            <i className="fab fa-instagram fa-2x me-2"></i>
+            <i className="fab fa-twitter fa-2x me-2"></i>
+            <i className="fab fa-facebook fa-2x me-2"></i>
+          </div>
+        </div>
+      </div>
+    </div>
   </footer>
-
 );
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,37 +3,73 @@ import { Link } from "react-router-dom";
 import LogoBarzanitas from "../../img/LogoBarzanitas.png";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<img src={LogoBarzanitas} alt="Logo de Barzanitas"></img>
-					<span className="navbar-brand mb-0 h1">Barzanitas</span>
-				</Link>
-				
-				
-				
-				<div className="dropdown">
-					
-						<button type="button" className="btn btn-primary me-3">Soy Cliente</button>
-						<button type="button" className="btn btn-success me-3">Soy Doctor</button>
-						<button type="button" className="btn btn-danger me-5">Cerrar Sesión</button>
-					
-						<a className="fa fa-bars fa-2x" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-						
-						</a>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link to="/" className="navbar-brand">
+          <img src={LogoBarzanitas} alt="Logo de Barzanitas" className="logo-img"></img>
+          <span className="navbar-text ms-2">Barzanitas</span>
+        </Link>
 
-					<ul className="dropdown-menu dropdown-menu-end p-3 fs-5">
-						<li><a className="dropdown-item fs-5 py-3" href="#">Especialidades</a></li>
-						<li><a className="dropdown-item fs-5 py-3" href="#">Doctores</a></li>
-						<li><a className="dropdown-item fs-5 py-3" href="#">La empresa</a></li>
-						<li><a className="dropdown-item fs-5 py-3" href="#">Contáctanos</a></li>
-						
-					</ul>
-				</div>
-				
-					
-			</div>
-		</nav>
-	);
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <button type="button" className="btn btn-primary me-3">
+                Soy Cliente
+              </button>
+            </li>
+            <li className="nav-item">
+              <button type="button" className="btn btn-success me-3">
+                Soy Doctor
+              </button>
+            </li>
+            <li className="nav-item">
+              <button type="button" className="btn btn-danger me-5">
+                Cerrar Sesión
+              </button>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i className="fa fa-bars fa-2x"></i>
+              </a>
+              <ul className="dropdown-menu dropdown-menu-end p-3 fs-5">
+                <li>
+                  <a className="dropdown-item fs-5 py-3" href="#">
+                    Especialidades
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item fs-5 py-3" href="#">
+                    Doctores
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item fs-5 py-3" href="#">
+                    La empresa
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item fs-5 py-3" href="#">
+                    Contáctanos
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
