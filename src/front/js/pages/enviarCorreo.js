5f07b82ab16a0681176a3fe7989916c257e55ab7
Diego Avila Ramos <145205871+avila46@users.noreply.github.com>
2024-03-06 18:33:07 +0000

    actualizacion y algo de estilos

--- src/front/js/pages/enviarCorreo.js
+++ src/front/js/pages/enviarCorreo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-import VistaEmail from "../component/vistaEnviarCorreo";
+import VistaEmail from "../component/vistaEmail";
 import "../../styles/color.css";
 
 const SendEmail = () => {
