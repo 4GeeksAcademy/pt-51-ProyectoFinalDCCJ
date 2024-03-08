6864a8d244e5922657588536f713b178fa186d94
cesarsan86 <86737129+cesarsan86@users.noreply.github.com>
2024-03-08 15:04:32 +0000

    modificación final

--- src/front/js/component/vistaHomeDoctores.js
+++ src/front/js/component/vistaHomeDoctores.js
@@ -6,17 +6,17 @@ import "../../styles/color.css";
 const VistaHomeDoctores = (props) => {
   return (
     <div className=" p-2">
-      <div className="col-md-12">
-          <div class="card text-center color3 mb-3 rounded-3" style={{width: "26rem" , height: "300px"}}>
+      
+          <div class="card text-center color3 mb-3 rounded-3" style={{width: "100%" , height: "600px"}}>
           <div className="row h-100">
-               <div className="col-md-6">
+               <div className="card-body p-2">
                  {props.imagen ? (
                    <img src={props.imagen} className="img-fluid rounded-start p-2" alt={`Foto de ${props.nombre}`} />
                  ) : (
                    <span>No hay imagen disponible</span>
                  )}
                </div>
-               <div className="col-md-6 d-flex align-items-center text-center">
+               <div className="mt-3">
                  <div className="card-body rounded-3 p-2">
                    <h5 className="card-title text-dark">{props.nombre}</h5>
                    <p className="card-title text-dark">{props.apellido}</p>
@@ -24,8 +24,8 @@ const VistaHomeDoctores = (props) => {
                </div>
              </div>
         </div>
-        </div>
     </div>
+     
 
     // <div className="container mt-3 ms-3">
     //   <div className="row">
